2379ddc7dd8186e8ffaf0acdd53929ccdf3cf2f7
Marcelo Lugard <79069502+mlugard@users.noreply.github.com>
2023-02-06 22:08:01 -0300

    Add files via upload

---
+++ Aula01.py
@@ -0,0 +1,13 @@
+# PANDAS é uma biblioteca de alto nível para fazer análise de dados práticas.
+# Objetivo: ferramenta de análise e maipulação de dados de código aberto mais poderosa e flexível disponível em qualquer idioma.'
+
+# ME LIVRANDO DA MALDIÇÃO
+""" print('Hello World!') """
+
+import pandas as pd
+
+# Primeiro exemplo de manipulação de dados usando Python Pandas:
+# Data: 06/02/2023
+
+# Data Frame
+combustiveis_df = pd.read_csv("precos-semestrais-ca-2022-01")
